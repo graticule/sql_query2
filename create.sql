4154d3993f92c214c5ff73998d01603c75008513
graticule <34333953+graticule@users.noreply.github.com>
2022-11-02 11:38:48 +0800

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,46 +1,51 @@
 CREATE TABLE IF NOT EXISTS genre(
-genre_id SERIAL PRIMARY KEY,
-genre_name VARCHAR(100) NOT NULL UNIQUE
+    PRIMARY KEY (genre_id),
+    genre_id   SERIAL,
+    genre_name VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS artist(
-artist_id SERIAL PRIMARY KEY,
-artist_name VARCHAR(200) NOT NULL
+    PRIMARY KEY (artist_id),
+    artist_id   SERIAL,
+    artist_name VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS genres_of_artists(
-artist_id INTEGER REFERENCES artist(artist_id),
-genre_id INTEGER REFERENCES genre(genre_id),
-PRIMARY KEY (artist_id, genre_id)
+    PRIMARY KEY (artist_id, genre_id),
+    artist_id INTEGER REFERENCES artist(artist_id),
+    genre_id  INTEGER REFERENCES genre(genre_id)
 );
 
 CREATE TABLE IF NOT EXISTS album(
-album_id SERIAL PRIMARY KEY,
-title VARCHAR(200) NOT NULL,
-release_year INTEGER NOT NULL CHECK (release_year > 1900)
+    PRIMARY KEY (album_id),
+    album_id     SERIAL,
+    title        VARCHAR(200) NOT NULL,
+    release_year INTEGER NOT NULL CHECK (release_year > 1900)
 );
 
 CREATE TABLE IF NOT EXISTS albums_of_artists(
-artist_id INTEGER REFERENCES artist(artist_id),
-album_id INTEGER REFERENCES album(album_id),
-PRIMARY KEY (artist_id, album_id)
+    PRIMARY KEY (artist_id, album_id),
+    artist_id INTEGER REFERENCES artist(artist_id),
+    album_id  INTEGER REFERENCES album(album_id)
 );
 
 CREATE TABLE IF NOT EXISTS track(
-track_id SERIAL PRIMARY KEY,
-title VARCHAR(200) NOT NULL,
-duration INTEGER NOT NULL CHECK (duration > 0),
-album_id INTEGER REFERENCES album(album_id)
+    PRIMARY KEY (track_id),
+    track_id SERIAL,
+    title    VARCHAR(200) NOT NULL,
+    duration INTEGER NOT NULL CHECK (duration > 0),
+    album_id INTEGER REFERENCES album(album_id)
 );
 
 CREATE TABLE IF NOT EXISTS collection(
-collection_id serial PRIMARY KEY,
-title VARCHAR(200) NOT NULL UNIQUE,
-release_year INTEGER NOT NULL CHECK (release_year > 1900)
+    PRIMARY KEY (collection_id),
+    collection_id SERIAL,
+    title         VARCHAR(200) NOT NULL UNIQUE,
+    release_year  INTEGER NOT NULL CHECK (release_year > 1900)
 );
 
-create table if not exists tracks_in_collections(
-track_id integer references track(track_id),
-collection_id integer references collection(collection_id),
-primary key (track_id, collection_id)
+CREATE TABLE IF NOT EXISTS tracks_in_collections(
+    PRIMARY KEY (track_id, collection_id),
+    track_id      INTEGER REFERENCES track(track_id),
+    collection_id INTEGER REFERENCES collection(collection_id)
 );
